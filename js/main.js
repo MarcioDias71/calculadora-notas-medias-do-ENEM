f09c04f56353061df64e7c61cd0b7a2b82330400
MarcioDias71 <marciodiasroodrigues@gmail.com>
2021-01-20 21:37:11 -0300

    make function calcAvarage

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+     const EventHandling = {
+    data() {
+      return {
+        message: ''
+     
+      }
+    },
+    methods: {
+      calcAverage() {
+        this.message = "Média Aritmética: "+(Number(lct.value) + Number(cht.value) + Number(cnt.value) + Number(mt.value) + Number(red.value))/5 ;
+        this.messagen = "Média Ponderada: "+ ((Number(lct.value) * Number(p1.value)) + (Number(cht.value) * Number(p2.value)) + (Number(cnt.value) * Number(p3.value)) + (Number(mt.value) * Number(p4.value))+(Number(red.value) * Number(p5.value)))/(Number(p1.value) + Number(p2.value) + Number(p3.value) + Number(p4.value) + Number(p5.value));
+      }
+    }
+  }
+  Vue.createApp(EventHandling).mount('#event-handling')
+ 
