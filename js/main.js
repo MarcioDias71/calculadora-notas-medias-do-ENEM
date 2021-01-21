5015cc52f1f697dc5498264e55a3625e277c40f2
MarcioDias71 <marciodiasroodrigues@gmail.com>
2021-01-20 21:52:40 -0300

    adding id for inputs

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@
     },
     methods: {
       calcAverage() {
-        this.message = "Média Aritmética: "+(Number(lct.value) + Number(cht.value) + Number(cnt.value) + Number(mt.value) + Number(red.value))/5 ;
-        this.messagen = "Média Ponderada: "+ ((Number(lct.value) * Number(p1.value)) + (Number(cht.value) * Number(p2.value)) + (Number(cnt.value) * Number(p3.value)) + (Number(mt.value) * Number(p4.value))+(Number(red.value) * Number(p5.value)))/(Number(p1.value) + Number(p2.value) + Number(p3.value) + Number(p4.value) + Number(p5.value));
+        this.message = " "+(Number(lct.value) + Number(cht.value) + Number(cnt.value) + Number(mt.value) + Number(red.value))/5 ;
+        this.messagen = " "+ ((Number(lct.value) * Number(p1.value)) + (Number(cht.value) * Number(p2.value)) + (Number(cnt.value) * Number(p3.value)) + (Number(mt.value) * Number(p4.value))+(Number(red.value) * Number(p5.value)))/(Number(p1.value) + Number(p2.value) + Number(p3.value) + Number(p4.value) + Number(p5.value));
       }
     }
   }
